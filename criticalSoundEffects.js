6144c9ecc12aa6492cd1c91df5191d4e7c84da99
Catlin Buckley <catlinbuckley@users.noreply.github.com>
2016-10-19 18:33:46 -0400

    fixed content undefined bug

--- criticalSoundEffects.js
+++ criticalSoundEffects.js
@@ -3,20 +3,20 @@ on('ready',function(){
     var excludeGM = true, // this to false if you wish to include GM rolls
         playerIds = [],
         players = findObjs({_type:'player'}) || [];
-
-
-    _.each(players,function (obj){
-        if(playerIsGM(obj.get('id')) && !excludeGM || !playerIsGM(obj.get('id'))) {
-            playerIds.push(obj.get('id'));
-        }
-    });
-
+        
+        
+        _.each(players,function (obj){
+            if(playerIsGM(obj.get('id')) && !excludeGM || !playerIsGM(obj.get('id'))) {
+                playerIds.push(obj.get('id'));
+            }
+        });
+        
     var allsongs = findObjs({
             _type: 'jukeboxtrack',
         }),
         criticalHit = {},
         criticalFail = {};
-
+        
     allsongs.forEach(function(song) {
         if(song.get('title') === 'Critical Hit') {
             criticalHit = song;
@@ -24,33 +24,34 @@ on('ready',function(){
             criticalFail = song;
         }
     });
-
+    
     on("chat:message", function(msg) {
         //for Shaped 5e Character Sheet
         if (msg.inlinerolls) {
             msg.inlinerolls.forEach(function(inlineRoll) {
                 criticalHitOrFail(inlineRoll.results);
             });
-        }
+        } 
+        
         //for roll chat command
-        else if (isJson(msg.content)) {
-            content = JSON.parse(msg.content);
+        else if (msg && isJson(msg.content)) {
+            var content = JSON.parse(msg.content);
             criticalHitOrFail(content);
-        }
+        } 
     });
-
+    
     function criticalHitOrFail(content) {
-        content.rolls.forEach(function(roll) {
-            if (roll.dice === 1 && roll.sides === 20) {
-                if (roll.results[0].v === 20) {
-                    play(criticalHit);
-                } else if (roll.results[0].v === 1) {
-                    play(criticalFail);
+            content.rolls.forEach(function(roll) {
+                if (roll.dice === 1 && roll.sides === 20) {
+                    if (roll.results[0].v === 20) {
+                        play(criticalHit);
+                    } else if (roll.results[0].v === 1) {
+                        play(criticalFail);
+                    }
                 }
-            }
-        });
-    }
-
+            });
+        }
+    
     function isJson(str) {
         try {
             JSON.parse(str);
@@ -59,7 +60,7 @@ on('ready',function(){
         }
         return true;
     }
-
+    
     function play (song) {
         song.set({'playing': true, 'softstop': false});
     }
