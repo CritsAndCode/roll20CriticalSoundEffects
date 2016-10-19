b8dc115c18c08d6c045f7a9c702281e9df3caad5
Catlin Buckley <catlin.buckley@gmail.com>
2016-10-19 15:05:40 -0400

    initial commit

---
+++ criticalHitOrFailSoundEffects.js
@@ -0,0 +1,71 @@
+on('ready',function(){
+    'use strict';
+    var excludeGM = true, // this to false if you wish to include GM rolls
+        playerIds = [],
+        players = findObjs({_type:'player'}) || [];
+
+
+    _.each(players,function (obj){
+        if(playerIsGM(obj.get('id')) && !excludeGM || !playerIsGM(obj.get('id'))) {
+            playerIds.push(obj.get('id'));
+        }
+    });
+
+    var allsongs = findObjs({
+            _type: 'jukeboxtrack',
+        }),
+        criticalHit = {},
+        criticalFail = {};
+
+    allsongs.forEach(function(song) {
+        if(song.get('title') === 'Critical Hit') {
+            criticalHit = song;
+        } else if (song.get('title') === 'Critical Fail') {
+            criticalFail = song;
+        }
+    });
+
+    on("chat:message", function(msg) {
+        log(playerIds.some(function(playerId){
+            return(playerId === msg.playerid);
+        }));
+        //for Shaped 5e Character Sheet
+        if (msg.inlinerolls) {
+            msg.inlinerolls.forEach(function(inlineRoll) {
+                criticalHitOrFail(inlineRoll.results);
+            });
+        }
+        //for roll chat command
+        else if (isJson(msg.content)) {
+            content = JSON.parse(msg.content);
+            criticalHitOrFail(content);
+        }
+    });
+
+    function criticalHitOrFail(content) {
+        content.rolls.forEach(function(roll) {
+            if (roll.dice === 1 && roll.sides === 20) {
+                if (roll.results[0].v === 20) {
+                    play(criticalHit);
+                } else if (roll.results[0].v === 1) {
+                    play(criticalFail);
+                }
+            }
+        });
+    }
+
+    function isJson(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
+    function play (song) {
+        song.set({'playing': true, 'softstop': false});
+    }
+
+    log('Script loaded: Critical Hit/Failure Sound Effects');
+});
